ee210a88fe1dc542c075ed8a4826b23a67122b6e
passioneffort <agronkercishta.ai@gmail.com>
2024-02-07 10:14:59 -0800

    changed

--- chaparral_pf-main/src/main/webapp/app/entities/menu.tsx
+++ chaparral_pf-main/src/main/webapp/app/entities/menu.tsx
@@ -2,13 +2,13 @@ import React from 'react';
 
 import MenuItem from 'app/shared/layout/menus/menu-item';
 
-// const EntitiesMenu = () => {
-//   return (
-//     <>
-//       {/* prettier-ignore */}
-//       {/* jhipster-needle-add-entity-to-menu - JHipster will add entities to the menu here */}
-//     </>
-//   );
-// };
+const EntitiesMenu = () => {
+  return (
+    <>
+      {/* prettier-ignore */}
+      {/* jhipster-needle-add-entity-to-menu - JHipster will add entities to the menu here */}
+    </>
+  );
+};
 
 export default EntitiesMenu;
